2bd43c545dc1ddc5b5aa36e7f08de25f166e85b4
Valter Lucena <valter.lucena@ccc.ufcg.edu.br>
2018-08-25 18:55:04 -0300

    adiciona base

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/', (req, res) => res.send('Got a POST request'))
+app.put('/user', (req, res) => res.send('Got a PUT request at /user'))
+app.delete('/user', (req, res) => res.send('Got a DELETE request at /user'))
+
+app.listen(3000, () => console.log('App running on port 3000'))
