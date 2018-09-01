639403768a0ebb5d26b5c8355481ccfb4ede83ec
Valter Lucena <valter.lucena@ccc.ufcg.edu.br>
2018-08-31 22:25:53 -0300

    modifica requisições

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var fs = require('fs')
 var morgan = require('morgan')
+var path = require('path')
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
 
-morgan('tiny')
+app.use(morgan('tiny', {stream: accessLogStream}))
 
 app.use('/static', express.static(__dirname+'/static'))
 
@@ -14,27 +17,27 @@ app.use('/', function(req, res, next){
 });
 
 app.get('/', (req, res) => {
-    res.send(JSON.stringify({value: 1}));
+    res.send(JSON.stringify({mensagem: 'olar'}));
 })
 
-app.get('/login', (req, res) => {
-    res.send(JSON.stringify({id: 1, nome: 'joao'}))
-})
-
-app.get('/aluno/:id/disciplina/:id', (req, res) => {
+app.get('/disciplina/:id', (req, res) => {
     res.send(JSON.stringify({cod: 123, nome: 'webdev'}))
 })
 
-app.get('/professor/:id', (req, res) => {
-    res.send(JSON.stringify({id: 12, nome: 'matheus'}))
+app.post('/disciplina/:id/atividade', (req, res) => {
+    res.send(JSON.stringify({cod: 789, descricao: 'atividade'}))
 })
 
-app.post('/professor/:id/criar-atividade/', (req, res) => {
-    res.send(JSON.stringify({cod: 4, descricao: 'definicoes'}))
+app.get('/disciplina/:id/atividade', (req, res) => {
+    res.send(JSON.stringify([{cod: 123, nome: 'web'}, {cod: 234, nome: 'dev'}]))
 })
 
-app.put('/professor/:id/atividade/:id', (req, res) =>{
-    res.send(JSON.stringify({cod: 4, descricao: 'definicoes de hj'}))
+app.get('/disciplina/:id/atividade/:id', (req, res) => {
+    res.send(JSON.stringify({cod: 456, descricao: 'atividade'}))
+})
+
+app.put('/disciplina/:id/atividade/:id', (req, res) => {
+    res.send(JSON.stringify({cod: 12, descricao: 'atividade respondida/editada'}))
 })
 
 app.listen(port, () => console.log(`App running on port ${port}`))
