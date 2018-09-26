721454d12dd61377eb91d2856b27d5b067e73909
valter.lucena@ccc.ufcg.edu.br <valter.lucena@ccc.ufcg.edu.br>
2018-09-26 16:17:45 -0300

    cors e passport

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const express   = require('express'),
     app         = express(),
     port        = process.env.PORT || 3000,
     swagger     = require('./docs/docsRoute'),
-    mongoose    = require('mongoose');
+    mongoose    = require('mongoose'),
+    cors        = require('cors'),
+    passport    = require('passport'),
+    session     = require('express-session');
 
 let accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 app.use(morgan('tiny', { stream: accessLogStream }))
@@ -15,6 +18,22 @@ app.use('/static', express.static(__dirname + '/static'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(cors());
+
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new localStrategy(
+    function(username, password, done) {
+        User.findOne({ username: username }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) { return done(null, false); }
+            if (!user.verifyPassword(password)) { return done(null, false); }
+        });
+    }
+));
 
 let quizz      = require('./api/quizz/quizzRoutes'),
     disciplina = require('./api/disciplina/disciplinaRoutes');
