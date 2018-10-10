015890a18fd95e16e993adf10aefcf79d0c69832
Valter Lucena <valter.lucena@ccc.ufcg.edu.br>
2018-10-10 00:01:57 -0300

    inicia documentacao

--- docs/swagger.js
+++ docs/swagger.js
@@ -4,9 +4,80 @@ const swaggerDef = {
     'basePath': '/',
     'host': 'localhost:3000',
     'info': {
-        'description': 'aprenda num flash com o Flexes',
+        'description': 'Sistema cujo objetivo é auxiliar na aprendizagem e revisão de conceitos utilizando flashcards.',
         'title': 'Flexes',
-        'version': '1.0.0'
+        'version': '1.0.0',
+        'license': {
+            'name': 'Flexes',
+            'url': 'https://github.com/valterlucena/devweb-back'
+        }
+    },
+    'consumes': [
+        'application/json'
+    ],
+    'produces': [
+        'application/json'
+    ],
+    'paths': {
+        '/usuario': {
+            'get': {
+                'tags': [
+                    'Usuario'
+                ],
+                'summary': 'Get all users',
+                'responses': {
+                    '200': {
+                        'description': 'OK',
+                        'schema': {
+                            '$ref': '#/definitions/Usuario'
+                        }
+                    }
+                }
+            },
+            'post': {
+                'tags': [
+                    'Usuario'
+                ],
+                'summary': 'Create a new user',
+                'parameters': [
+                    {
+                        'name': 'username',
+                        'in': 'body',
+                        'description': 'Username of the user that we want to create.',
+                        'required': true,
+                        'schema': {
+                            '$ref': '#/definitions/Usuario'
+                        }
+                    },
+                    {
+                        'name': 'password',
+                        'in': 'body',
+                        'description': 'Password of the user that we want to create.',
+                        'required': true,
+                        'schema': {
+                            '$ref': '#/definitions/Usuario'
+                        }
+                    },
+                    {
+                        'name': 'type',
+                        'in': 'body',
+                        'description': "Type of the user that we want to create. It can be 'aluno' or 'professor'",
+                        'required': true,
+                        'schema': {
+                            '$ref': '#/definitions/Usuario'
+                        }
+                    }
+                ],
+                'produces': [
+                    'application/json'
+                ], 
+                'responses': {
+                    '201': {
+                        'description': "{'resul': {New Usuario}}"
+                    }
+                }
+            }
+        }
     }
 };
 
